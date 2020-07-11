898e9d21db01b3019879f097633f044669f57d66
Stephen Tanksley <stephen.tanksley@gmail.com>
2020-07-11 13:58:27 -0500

    Updated searching.py.

--- src/searching/searching.py
+++ src/searching/searching.py
@@ -4,9 +4,12 @@ def binary_search(arr, target, start, end):
     U - Understand. A binary search performs by cutting the array in half every time
         until it reaches its target. In a recursive binary search, we use recursion
         to determine which half of the array to discard before trying again.
+
     P - Will require a base case where the length of the array is 0, a case where 
         the mid-point matches the target, a recursive case. If we find the target, we will return the index of the array where we found it. Else, we return -1.
-    E - Write base case first, then consider recursive cases. Base cases involve 
+
+    E - Write base case first, then consider recursive cases.
+
     R - Get it working first, then look for ways to optimize.
     """
 
@@ -23,6 +26,8 @@ def binary_search(arr, target, start, end):
         #   if the array with an index in the middle is equal to the target,
         #   return index of the item to show its location.
         if arr[mid] == target:
+            # print(mid)
+            # print(arr.index(arr[mid]))
             return arr.index(arr[mid])
 
         # recursive cases
@@ -48,3 +53,13 @@ def binary_search(arr, target, start, end):
 def agnostic_binary_search(arr, target):
     # Your code here
     pass
+
+
+"""
+    To implement an order agnostic search, the same principles apply with some variations.
+    
+    1) We need to detect if the list is in ascending or descending order.
+    2) We need to implement search cases for both of them.
+    3) We need to have those cases trigger by the correctly detected list order.
+
+"""
